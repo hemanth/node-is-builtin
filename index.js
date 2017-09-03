adb1b78c66be5d4c0d7eedeeba620a944af65e8e
Benjamin Gruenbaum <inglor@gmail.com>
2017-09-04 02:43:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-module.exports = function (module) {
-  'use strict';
-  var builtins = require('repl')._builtinLibs;
-  return builtins.includes(module);
-};
+module.exports = (module) => require('repl')._builtinLibs.includes(module);
