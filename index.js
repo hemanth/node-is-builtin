c68d02a915644ad6e8c45fc63d72b0a175a4578a
Benjamin Gruenbaum <inglor@gmail.com>
2017-09-04 02:41:33 +0300

    refactor to includes
    No reason to do the confusing !!~ thing for a while now :)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function (module) {
   'use strict';
   var builtins = require('repl')._builtinLibs;
-  return !!~builtins.indexOf( module );
+  return builtins.includes(module);
 };
