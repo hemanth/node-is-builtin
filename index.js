3dccd996d9574712241389837af700399fc08013
oriash93 <oriash93@gmail.com>
2017-10-15 20:27:44 +0300

    Refactor index.js to use includes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const builtinModules = require('builtin-modules');
+
 module.exports = function (module) {
   'use strict';
-  var builtins = require('repl')._builtinLibs;
-  return !!~builtins.indexOf( module );
+  return builtinModules.includes(module);
 };
